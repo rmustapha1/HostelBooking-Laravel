f4df251743c8460553f9c37c8df4c3824bbb5304
Rafick Mustapha <mustapharafick3@gmail.com>
2023-10-30 08:58:23 +0000

    curl update

--- app/Notifications/PaymentNotification.php
+++ app/Notifications/PaymentNotification.php
@@ -14,15 +14,50 @@ class PaymentNotification extends Notification
 
         $client = new Client();
 
-        $response = $client->post('https://mnotify.com/api/smsapi', [
-            'form_params' => [
-                'key' => 'bQWU97WiwiYX4awDiuESK4qzt',
-                'to' => $phone,
-                'msg' => $message,
-                'sender_id' => 'Prvt-Hostel',
-            ],
-        ]);
+        // $response = $client->post('https://mnotify.com/api/smsapi/', [
+        //     'form_params' => [
+        //         'key' => 'bQWU97WiwiYX4awDiuESK4qzt',
+        //         'to' => $phone,
+        //         'msg' => $message,
+        //         'sender_id' => 'Prvt-Hostel',
+        //     ],
+        // ]);
+        // $response = $client->post('https://api.mnotify.com/api/sms/quick', [
+        //     'form_params' => [
+        //         'key' => 'bQWU97WiwiYX4awDiuESK4qzt',
+        //         'sender' => 'Prvt-Hostel',
+        //         'recipient' => $phone,
+        //         'message' => $message,
+        //         'is_schedule' => 'false',
+        //         'schedule_date' => ''
+        //     ],
+        // ]);
 
-        return $response->getBody();
+        $endPoint = 'https://api.mnotify.com/api/sms/quick';
+        // $apiKey = '4I7V9voJOgR2sWY7wg9gkjBtZ';
+        $apiKey = 'WGq00aWm6vwnJkBoKixSNnjbr';
+        $url = $endPoint . '?key=' . $apiKey;
+        $data = [
+            'sender' => 'Prvt-Hostel',
+            'recipient' => $phone,
+            'message' => $message,
+            'is_schedule' => 'false',
+            'schedule_date' => ''
+        ];
+
+        $ch = curl_init();
+        $headers = array();
+        $headers[] = "Content-Type: application/json";
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $result = curl_exec($ch);
+        $result = json_decode($result, TRUE);
+        var_dump($result);
+        curl_close($ch);
+
+        // return $response->getBody();
     }
 }
